f257d7fac927777761efc852b588486dd62a3bf9
Marcos Dione <mdione@grulic.org.ar>
2013-03-27 00:42:15 +0100

    * more steps for migration into a database.

--- layers.sh
+++ layers.sh
@@ -1,4 +1,7 @@
 # layers="roads waterways railways points natural places"
 # roads waterways points natural places railways
 
-layers="natural waterways landuse roads railways points places"
+ #layers="natural waterways landuse roads railways points places"
+
+ highways="secondary_link secondary primary_link primary trunk_link trunk motorway_link motorway"
+ 
