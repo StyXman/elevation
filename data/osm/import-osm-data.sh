088ea5e1f018a32b49833229529bbe95ef74b3ef
Marcos Dione <mdione@grulic.org.ar>
2016-03-04 10:02:41 +0100

    [+] you can specify target -d|--database.

--- data/osm/import-osm-data.sh
+++ data/osm/import-osm-data.sh
@@ -6,29 +6,38 @@ set -eu
 # cache
 # procs
 # path
-common_opts="--username $USER --database gis --cache 2048 --number-processes 4 \
+
+db='gis'
+
+case "$1" in
+  -d|--database)
+    db="$2"
+    shift 2
+esac
+
+common_opts="--username $USER --database "$db" --cache 4096 --number-processes 2 \
     --verbose --slim --flat-nodes /home/mdione/src/projects/osm/nodes.cache \
-    --style import.style"
+    --style import.style --drop --tablespace-slim-data index"
 
 command=$1
 shift
 
 case "$command" in
   restart)
-    sudo -u postgres dropdb --if-exists gis
-    sudo -u postgres createdb -E UTF8 -O $USER gis
-    # sudo -u postgres createlang plpgsql gis
-    sudo -u postgres psql -d gis -c "CREATE EXTENSION postgis;"
-    # sudo -u postgres psql -d gis -c "CREATE EXTENSION hstore;"
-    sudo -u postgres psql -d gis -c "ALTER TABLE geometry_columns OWNER TO $USER;"
-    sudo -u postgres psql -d gis -c "ALTER TABLE spatial_ref_sys  OWNER TO $USER;"
-    # sudo -u postgres psql -d gis -f /usr/share/osm2pgsql/900913.sql
+    sudo --user postgres dropdb --if-exists "$db"
+    sudo --user postgres createdb --encoding UTF8 --owner $USER "$db"
+    # sudo --user postgres createlang plpgsql "$db"
+    sudo --user postgres psql --dbname "$db" --command "CREATE EXTENSION postgis;"
+    # sudo --user postgres psql --dbname "$db" --command "CREATE EXTENSION hstore;"
+    sudo --user postgres psql --dbname "$db" --command "ALTER TABLE geometry_columns OWNER TO $USER;"
+    sudo --user postgres psql --dbname "$db" --command "ALTER TABLE spatial_ref_sys  OWNER TO $USER;"
+    # sudo --user postgres psql --dbname "$db" --file /usr/share/osm2pgsql/900913.sql
     ;;
 
   import)
     opts="--create --unlogged"
     nice -n 19 osm2pgsql $opts $common_opts "$@"
-    psql -d gis -c "create index planet_osm_point_populaiton_index on planet_osm_point (cast (population as int) desc nulls last);"
+    # psql --dbname "$db" --command "create index planet_osm_point_population_index on planet_osm_point (cast (population as int) desc nulls last);"
     ;;
 
   append)
@@ -39,6 +48,6 @@ case "$command" in
     ;;
 
   *)
-    echo "ERROR: wrong ocmmand $command"
+    echo "ERROR: wrong command $command"
     exit 1
 esac
