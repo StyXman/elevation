9054d7d617d39298c91669e0cb328e75c0e13017
Marcos Dione <mdione@grulic.org.ar>
2016-05-09 18:17:56 +0200

    [*] major rewrite, API change, the script downloads only one extract to the DST dir.

--- data/osm/pull_osm_data.sh
+++ data/osm/pull_osm_data.sh
@@ -2,22 +2,26 @@
 
 set -o errexit -o nounset
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 EXTRACT [...]"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 EXTRACT DST"
     echo
     echo -e "\tEXTRACT can be in the form of europe, europe/france or europe/france/provence-alps-cote-d-azur"
+    echo -e "\tDST is the destination directory"
     echo -e "\tFor more info, go to http://download.geofabrik.de/"
     exit 1
 fi
 
-for extract in $@; do
-    path="$extract-latest.osm.pbf"
-    file=$(basename $path)
+extract=$1
+dst=$2
 
-    if [ -f $file ]; then
-        time_cond="--time-cond $file"
-    else
-        time_cond=""
-    fi
-    curl $time_cond --location --output $file http://download.geofabrik.de/$path
-done
+path="${extract}-latest.osm.pbf"
+file="$dst/$(basename $path)"
+
+mkdir -pv $dst
+
+if [ -f $file ]; then
+    time_cond="--time-cond $file"
+else
+    time_cond=""
+fi
+curl $time_cond --location --output $file http://download.geofabrik.de/$path
