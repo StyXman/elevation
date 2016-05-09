af5c9f59c018c281738357641a11458f96ba288d
Marcos Dione <mdione@grulic.org.ar>
2016-05-09 17:14:18 +0200

    [+] added usage.

--- data/osm/pull_osm_data.sh
+++ data/osm/pull_osm_data.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 EXTRACT [...]"
+    echo
+    echo -e "\tEXTRACT can be europe, europe/france or europe/france/provence-alps-cote-d-azur"
+    echo -e "\tFor more info, go to http://download.geofabrik.de/"
+    exit 1
+fi
+
 for i in $@; do
     wget --continue http://download.geofabrik.de/$i-latest.osm.pbf
 done
