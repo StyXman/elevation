09717b7aa8aa767b63705c08f189d2817d8bdcb8
Marcos Dione <mdione@grulic.org.ar>
2016-05-09 17:57:45 +0200

    [*] s/wget/curl/, with lots of improvements.

--- data/osm/pull_osm_data.sh
+++ data/osm/pull_osm_data.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 
+set -o errexit -o nounset
+
 if [ $# -eq 0 ]; then
     echo "Usage: $0 EXTRACT [...]"
     echo
-    echo -e "\tEXTRACT can be europe, europe/france or europe/france/provence-alps-cote-d-azur"
+    echo -e "\tEXTRACT can be in the form of europe, europe/france or europe/france/provence-alps-cote-d-azur"
     echo -e "\tFor more info, go to http://download.geofabrik.de/"
     exit 1
 fi
 
-for i in $@; do
-    wget --continue http://download.geofabrik.de/$i-latest.osm.pbf
+for extract in $@; do
+    path="$extract-latest.osm.pbf"
+    file=$(basename $path)
+
+    if [ -f $file ]; then
+        time_cond="--time-cond $file"
+    else
+        time_cond=""
+    fi
+    curl $time_cond --location --output $file http://download.geofabrik.de/$path
 done
