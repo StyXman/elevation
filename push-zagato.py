1904125baa0bba3a54db0298951754a05a123891
Marcos Dione <mdione@grulic.org.ar>
2016-05-12 21:27:57 +0200

    [*] use argparse instead.

--- push-zagato.py
+++ push-zagato.py
@@ -7,6 +7,7 @@ from shutil import copy, rmtree
 from os import listdir, stat, unlink, mkdir, walk, makedirs
 from os.path import dirname, basename, join as path_join
 from errno import ENOENT, EEXIST
+import argparse
 
 def file_newer (src, dst):
     try:
@@ -28,27 +29,22 @@ def file_newer (src, dst):
     # print src_stat.st_mtime, dst_stat.st_mtime
     return src_stat.st_mtime>dst_stat.st_mtime
 
-if len (sys.argv)==0 or sys.argv[1] in ('-h', '--help'):
-    # usage
-    print """%s [-f|-o|--force|--overwrite] SRC DST MAP...""" % sys.argv[0]
-    exit (0)
+parser= argparse.ArgumentParser ()
+parser.add_argument ('-f', '--force',     dest='overwrite', action='store_true')
+parser.add_argument ('-o', '--overwrite', dest='overwrite', action='store_true')
+parser.add_argument ('src', metavar='SRC')
+parser.add_argument ('dst', metavar='DST')
+parser.add_argument ('maps', metavar='MAP', nargs='+')
+opts= parser.parse_args ()
 
-overwrite= False
-if sys.argv[1] in ('-f', '-o', '--force', '--overwrite'):
-    overwrite= True
-    sys.argv.pop (1)
-
-src= sys.argv.pop (1)
-dst= sys.argv.pop (1)
-sectors= sys.argv[1:]
-atlas= map_utils.Atlas (sectors)
+atlas= map_utils.Atlas (opts.maps)
 
 # I assume I will want all the zoom levels
 # so I don't have to check for dir contents
 for z in range (atlas.minZoom, atlas.maxZoom+1):
     # print ("- z", (z, ))
     try:
-        present_x= listdir (path_join (dst, str (z)))
+        present_x= listdir (path_join (opts.dst, str (z)))
     except OSError as e:
         if e.errno!=ENOENT:
             raise e
@@ -56,14 +52,14 @@ for z in range (atlas.minZoom, atlas.maxZoom+1):
         for x in ( int (x) for x in present_x ):
             # print ("-- dx", (z, x))
             if not (z, x) in atlas:
-                d= path_join (dst, str (z), str (x))
+                d= path_join (opts.dst, str (z), str (x))
                 rmtree (d)
                 print ("X: %s" % d)
 
     for x in atlas.iterate_x (z):
         # print ("-- sx", (z, x))
         try:
-            present_y= listdir (path_join (dst, str (z), str (x)))
+            present_y= listdir (path_join (opts.dst, str (z), str (x)))
         except OSError as e:
             if e.errno!=ENOENT:
                 raise e
@@ -71,18 +67,18 @@ for z in range (atlas.minZoom, atlas.maxZoom+1):
             for y in ( int (y.split ('.')[0]) for y in present_y):
                 # print ("--- dy", (z, x, y))
                 if not (z, x, y) in atlas:
-                    f= path_join (dst, str (z), str (x), str (y)+'.png')
+                    f= path_join (opts.dst, str (z), str (x), str (y)+'.png')
                     unlink (f)
                     print ("D: %s" % f)
 
         for y in atlas.iterate_y (z, x):
             # print ("--- sy", (z, x, y))
-            src_y= path_join (src, str (z), str (x), str (y)+'.png')
-            dst_y= path_join (dst, str (z), str (x), str (y)+'.png')
+            src_y= path_join (opts.src, str (z), str (x), str (y)+'.png')
+            dst_y= path_join (opts.dst, str (z), str (x), str (y)+'.png')
 
             if file_newer (src_y, dst_y) or overwrite:
                 try:
-                    map_utils.makedirs (path_join (dst, str (z), str (x)))
+                    map_utils.makedirs (path_join (opts.dst, str (z), str (x)))
                     copy (src_y, dst_y)
                     print ("C: %s -> %s" % (src_y, dst_y))
                 except Exception as e:
