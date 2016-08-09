6694a78d2297487203a0ed7f4655555a8bb340f2
Marcos Dione <mdione@grulic.org.ar>
2016-08-09 12:18:00 +0200

    [*] moved user and hdd tablespace creation to the boot verb.

--- data/osm/import-osm-data.sh
+++ data/osm/import-osm-data.sh
@@ -8,6 +8,8 @@ set -eu
 # path
 
 db='gis'
+# bin='osm2pgsql'
+bin='/home/mdione/src/system/osm/osm2pgsql/build/osm2pgsql'
 
 case "$1" in
   -d|--database)
@@ -15,20 +17,26 @@ case "$1" in
     shift 2
 esac
 
-common_opts="--username $USER --database "$db" --cache 4096 --number-processes 2 \
+common_opts="--username $USER --database "$db" --cache 8192 --number-processes 2 \
     --verbose --slim --flat-nodes /home/mdione/src/projects/osm/nodes.cache \
-    --style import.style --drop --tablespace-slim-data index"
+    --style import.style --drop"
 
 command=$1
 shift
 
 case "$command" in
+  boot)
+    sudo --user postgres createuser --superuser mdione
+    sudo --user postgres psql -c "create tablespace hdd owner mdione location '/var/lib/data/postgresql';"
+    ;;
+
   restart)
     sudo --user postgres dropdb --if-exists "$db"
     sudo --user postgres createdb --encoding UTF8 --owner $USER "$db"
     # sudo --user postgres createlang plpgsql "$db"
     sudo --user postgres psql --dbname "$db" --command "CREATE EXTENSION postgis;"
-    # sudo --user postgres psql --dbname "$db" --command "CREATE EXTENSION hstore;"
+    sudo --user postgres psql --dbname "$db" --command "CREATE EXTENSION postgis_sfcgal;"
+    sudo --user postgres psql --dbname "$db" --command "CREATE EXTENSION hstore;"
     sudo --user postgres psql --dbname "$db" --command "ALTER TABLE geometry_columns OWNER TO $USER;"
     sudo --user postgres psql --dbname "$db" --command "ALTER TABLE spatial_ref_sys  OWNER TO $USER;"
     # sudo --user postgres psql --dbname "$db" --file /usr/share/osm2pgsql/900913.sql
@@ -36,15 +44,13 @@ case "$command" in
 
   import)
     opts="--create --unlogged"
-    nice -n 19 osm2pgsql $opts $common_opts "$@"
+    nice -n 19 $bin $opts $common_opts "$@"
     # psql --dbname "$db" --command "create index planet_osm_point_population_index on planet_osm_point (cast (population as int) desc nulls last);"
     ;;
 
   append)
     opts="--append"
-    for file in $@; do
-        nice -n 19 osm2pgsql $opts $common_opts $file
-    done
+    nice -n 19 $bin $opts $common_opts "$@"
     ;;
 
   *)
