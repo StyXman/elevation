2f654dbc374cc08c6de0feacb8df1a19945d8a97
Marcos Dione <mdione@grulic.org.ar>
2017-04-25 08:53:45 +0200

    [*] Don't play with branches anymore, it's a mess.

--- render.sh
+++ render.sh
@@ -2,12 +2,9 @@
 
 set -eu
 
-git checkout render
 make Elevation.xml
 # git commit fails if there's nothing to commit
 git commit Elevation.xml -m "[*] $(date --iso-8601=seconds)." &> /dev/null || true
 
 ./generate_tiles.py --input-file Elevation.xml --output-dir Elevation \
     --metatile-size 8 "$@" || true  # I hope this handles C-c
-
-git checkout master
