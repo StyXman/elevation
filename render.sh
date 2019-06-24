1fd717441731b3483231a6f753afae6d02d4f835
Marcos Dione <mdione@grulic.org.ar>
2019-06-24 09:39:31 +0200

    Do not use local mapnik, it has some bugs and it's slower.

--- render.sh
+++ render.sh
@@ -6,7 +6,10 @@ make Elevation.xml
 # git commit fails if there's nothing to commit
 git commit Elevation.xml -m "[*] $(date --iso-8601=seconds)." &> /dev/null || true
 
-nice -n 19 python3.6 -s ./generate_tiles.py \
+PYTHON="python3 -s"
+# PYTHON="LD_LIBRARY_PATH=$HOME/local/lib pyhton3"
+
+nice -n 19 $PYTHON ./generate_tiles.py \
     --input-file Elevation.xml \
     --output-dir Elevation \
     --metatile-size 8 --log-file render.log "$@"
