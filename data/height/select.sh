902ba63b2fb59aa8638fa948adc45ba123c17dc7
Marcos Dione <mdione@grulic.org.ar>
2019-08-22 21:53:00 +0200

    Build the summary files if needed before switching.

--- data/height/select.sh
+++ data/height/select.sh
@@ -2,9 +2,17 @@
 
 set -eu
 
-for layer in hillshade terrain slopeshade; do
-    ln -sfv $1-$layer.vrt mixed-$layer.vrt
+name=$1
+
+make $(for layer in hillshade terrain slopeshade; do
     for size in medium small; do
-        ln -sfv $1-$layer-$size.tif mixed-$layer-$size.tif
+        echo $name-$layer-$size.tif
+    done
+done)
+
+for layer in hillshade terrain slopeshade; do
+    ln -sfv $name-$layer.vrt mixed-$layer.vrt
+    for size in medium small; do
+        ln -sfv $name-$layer-$size.tif mixed-$layer-$size.tif
     done
 done
