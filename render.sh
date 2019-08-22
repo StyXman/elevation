9cdb3cd3b3861edcfc4fe5f65f5bb4f4d86bc132
Marcos Dione <mdione@grulic.org.ar>
2019-08-22 21:48:38 +0200

    Make sure Elevation.xml is not empty.

--- render.sh
+++ render.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if ! [ -s Elevation.xml ]; then
+    # a previous run mangled the file
+    rm -f Elevation.xml
+fi
+
 make Elevation.xml
 # git commit fails if there's nothing to commit
 git commit Elevation.xml -m "[*] $(date --iso-8601=seconds)." &> /dev/null || true
